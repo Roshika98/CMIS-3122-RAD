2eb521929e41b8b29b692e3a53dc35fc82b27ca2
Roshika Perera <pereraroshika98@gmail.com>
2022-05-10 20:38:33 +0530

    seeder file updated

--- database/seeder.js
+++ database/seeder.js
@@ -2,15 +2,18 @@ const sql = require('mysql2/promise');
 const container = require('./data/dataHolder');
 
 var createDept = 'CREATE TABLE departments(deptID INT NOT NULL PRIMARY KEY,deptName VARCHAR(50) NOT NULL)';
-var createModules = 'CREATE TABLE modules(course_code VARCHAR(10) NOT NULL PRIMARY KEY, name VARCHAR(50) NOT NULL,credit INT NOT NULL,level INT NOT NULL,semester INT NOT NULL,deptID NOT NULL,desc VARCHAR(255),CONSTRAINT fk_dept FOREIGN KEY deptID REFERENCES departments(deptID) ON DELETE CASCADE ON UPDATE CASCADE)';
-var createGeneral = 'CREATE TABLE general(course_code VARCHAR(10) NOT NULL,availability BOOLEAN NOT NULL,mandatory BOOLEAN NOT NULL,CONSTRAINT fk_gencourseCode FOREIGN KEY course_code REFERENCES modules(course_code) ON DELETE CASCADE ON UPDATE CASCADE)';
-var createJM = 'CREATE TABLE jointMajor(course_code VARCHAR(10) NOT NULL,m1_availability BOOLEAN NOT NULL,m1_mandatory BOOLEAN NOT NULL,m2_availabilty BOOLEAN NOT NULL,m2_mandatory BOOLEAN NOT NULL,CONSTRAINT fk_jmcourseCode FOREIGN KEY course_code REFERENCES modules(course_code) ON DELETE CASCADE ON UPDATE CASCADE)';
-var createSpecial = 'CREATE TABLE special(course_code VARCHAR(10) NOT NULL,availabilty BOOLEAN NOT NULL,CONSTRAINT fk_spcourseCode FOREIGN KEY course_code REFERENCES modules(course_code) ON DELETE CASCADE ON UPDATE CASCADE)';
+var createModules = 'CREATE TABLE modules(course_code VARCHAR(10) NOT NULL PRIMARY KEY, name VARCHAR(50) NOT NULL,credit INT NOT NULL,level INT NOT NULL,semester INT NOT NULL,deptID INT NOT NULL,description TEXT,CONSTRAINT fk_dept FOREIGN KEY (deptID) REFERENCES departments(deptID) ON DELETE CASCADE ON UPDATE CASCADE)';
+var createGeneral = 'CREATE TABLE general(course_code VARCHAR(10) NOT NULL,availability BOOLEAN NOT NULL,mandatory BOOLEAN NOT NULL,CONSTRAINT fk_gencourseCode FOREIGN KEY (course_code) REFERENCES modules(course_code) ON DELETE CASCADE ON UPDATE CASCADE)';
+var createJM = 'CREATE TABLE jointMajor(course_code VARCHAR(10) NOT NULL,m1_availability BOOLEAN NOT NULL,m1_mandatory BOOLEAN NOT NULL,m2_availability BOOLEAN NOT NULL,m2_mandatory BOOLEAN NOT NULL,CONSTRAINT fk_jmcourseCode FOREIGN KEY (course_code) REFERENCES modules(course_code) ON DELETE CASCADE ON UPDATE CASCADE)';
+var createSpecial = 'CREATE TABLE special(course_code VARCHAR(10) NOT NULL,availability BOOLEAN NOT NULL,mandatory BOOLEAN NOT NULL,CONSTRAINT fk_spcourseCode FOREIGN KEY (course_code) REFERENCES modules(course_code) ON DELETE CASCADE ON UPDATE CASCADE)';
 
 var insertDept = 'INSERT INTO departments(deptID,deptName) VALUES (?,?)';
-var insertModules = 'INSERT INTO modules(course_code,name,credit,level,semester,deptID,desc) VALUES (?,?,?,?)';
+var insertModules = 'INSERT INTO modules(course_code,name,credit,level,semester,deptID,description) VALUES (?,?,?,?,?,?,?)';
+var insertSpecial = 'INSERT INTO special(course_code,availability,mandatory) VALUES (?,?,?)';
+var insertJM = 'INSERT INTO jointMajor(course_code,m1_availability,m1_mandatory,m2_availability,m2_mandatory) VALUES (?,?,?,?,?)';
+var insertGeneral = 'INSERT INTO general(course_code,availability,mandatory) VALUES (?,?,?)';
 
-
+var desc = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec et scelerisque urna, sed placerat mi. Nunc ac erat finibus nibh dictum dictum. In sed ex nec arcu vestibulum commodo. Nullam ut velit nec sem ultricies imperdiet iaculis quis erat. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse tempor pharetra tellus et rhoncus.';
 
 
 async function createTables() {
@@ -26,7 +29,6 @@ async function createTables() {
         var q4 = await connection.query(createJM);
         var q5 = await connection.query(createSpecial);
         const ending = await connection.end();
-        console.log('Tables created......');
     } catch (error) {
         console.log(error);
     }
@@ -40,14 +42,23 @@ async function addData() {
             user: 'root',
             database: 'fas'
         });
-        cmis.forEach(element => {
-            execution(insertCMIS, connection, element);
+        container.deptData.forEach(element => {
+            execution(insertDept, connection, element);
+        });
+        console.log('Department data added...............');
+        container.modulesData.forEach(element => {
+            execution(insertModules, connection, [element[0], element[1], element[2], element[3], element[4], element[5], desc]);
+            if (element[3] == 3) {
+                execution(insertGeneral, connection, [element[0], element[12], element[13]]);
+            } if (element[3] > 2) {
+                execution(insertJM, connection, [element[0], element[8], element[9], element[10], element[11]]);
+                execution(insertSpecial, connection, [element[0], element[6], element[7]]);
+            }
         });
         connection.end();
     } catch (error) {
         console.log(error);
     }
-
 }
 
 async function execution(q, connection, values) {
@@ -55,9 +66,21 @@ async function execution(q, connection, values) {
     return result;
 }
 
-// addData();
+async function runQueries() {
+    try {
+        var q = await createTables().then(x => {
+            console.log('Tables Created................');
+        });
+        var q2 = await addData().then(x => {
+            console.log('Data entered into modules table.................');
+        });
+    } catch (error) {
+        console.log(error);
+    }
+}
 
-// createTables();
+
+runQueries();
 
 
 
