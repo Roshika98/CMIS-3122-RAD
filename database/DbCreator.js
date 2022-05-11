b8422364d54f548fc9e27994a94bbe6d98301fb2
Roshika Perera <pereraroshika98@gmail.com>
2022-05-11 09:06:01 +0530

    helper file to create a database & remove it

---
+++ database/DbCreator.js
@@ -0,0 +1,57 @@
+const db = require('mysql2/promise');
+
+var q = 'CREATE DATABASE testingDB';
+
+var connection = null;
+
+async function establishConnection() {
+    try {
+        connection = await db.createConnection({
+            host: 'localhost',
+            user: 'root'
+        });
+        if (connection) console.log('çonnection acquired............');
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+
+async function Create() {
+    try {
+        const result = await connection.query(q);
+        if (result) console.log('testingDB Database created...............');
+        // console.log(result);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+
+async function dropDatabase() {
+    try {
+        const result = await connection.query('DROP DATABASE IF EXISTS testingDB');
+        if (result) console.log('Removed Database testingDB................');
+        // console.log(result);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+
+//! Run this to create a database
+
+
+// establishConnection().then(e => {
+//     Create();
+//     connection.end();
+// });
+
+
+//! Run this to Remove the Database
+
+
+// establishConnection().then(e => {
+//     dropDatabase();
+//     connection.end();
+// });
