cf11429461e02c9c9f6e7c898df8d7a4acf65ba1
Roshika Perera <pereraroshika98@gmail.com>
2022-05-11 13:34:42 +0530

    form handler updated prt 2

--- public/javaScript/formHandler.js
+++ public/javaScript/formHandler.js
@@ -1,17 +1,25 @@
 
-// Variables-------------------------------------------------------
+//! Variables-------------------------------------------------------
 
 var currTab = 0;
 var prevBtn = document.getElementById('previous');
 var nextBtn = document.getElementById('next');
 var submitBtn = document.getElementById('submit');
+var levelSelector = document.getElementById('levelSelect');
+var degreeSelector = document.getElementById('degreeType');
+
+var set1 = document.getElementById('combSet1');
+var set2 = document.getElementById('combSet2');
+
 var generalComb = document.getElementById('General');
 var jointComb = document.getElementById('JM');
 var specialComb = document.getElementById('special');
+
+
 const content = document.getElementsByClassName('tab');
+const degreetypes = [generalComb, jointComb, specialComb];
 
-
-// Button events----------------------------------------------------
+//! Events----------------------------------------------------
 
 nextBtn.addEventListener('click', async (event) => {
     event.preventDefault();
@@ -23,10 +31,19 @@ prevBtn.addEventListener('click', (event) => {
     event.preventDefault();
     event.stopPropagation();
     Showcontent(-1);
-})
+});
 
 
-// Function declarations & calls-------------------------------------
+levelSelector.addEventListener('change', e => {
+    ShowLevelContent(levelSelector.value);
+});
+
+degreeSelector.addEventListener('change', e => {
+    ShowDegreeType(degreeSelector.value);
+});
+
+
+//! Function declarations & calls-------------------------------------
 
 HideAll();
 ShowButtons(currTab);
@@ -61,11 +78,35 @@ function Showcontent(n) {
 }
 
 
+function ShowLevelContent(n) {
+    if (n > 0 && n <= 2) {
+        set1.style.display = '';
+        set2.style.display = 'none';
+    } else if (n <= 4) {
+        set1.style.display = 'none';
+        set2.style.display = '';
+        ShowDegreeType(degreeSelector.value);
+    } else {
+        set1.style.display = 'none';
+        set2.style.display = 'none';
+    }
+}
+
+function ShowDegreeType(n) {
+    for (var i = 0; i < degreetypes.length; i++) {
+        if (i == (n - 1)) degreetypes[i].style.display = '';
+        else degreetypes[i].style.display = 'none';
+    }
+}
+
+
 function HideAll() {
 
     for (var i = 0; i < content.length; i++) {
         content[i].style.display = 'none';
     }
+    set1.style.display = 'none';
+    set2.style.display = 'none';
 }
 
 
