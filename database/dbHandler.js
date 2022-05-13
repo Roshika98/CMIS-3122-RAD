f9aa18b8b3d49c3b8cdd9eec28f909824eec8952
Roshika Perera <pereraroshika98@gmail.com>
2022-05-13 13:37:21 +0530

    modules search implemented

--- database/dbHandler.js
+++ database/dbHandler.js
@@ -2,18 +2,28 @@ const connection = require('./dbConnector');
 
 class DbHandler {
 
-    // connection = require('./dbConnector');
-    static async getmodules() {
-        var q = 'SELECT * FROM modules';
+
+    static async getmodulesDetails(dept, lvl) {
+        var q = 'SELECT course_code,name,credit,semester,description FROM modules INNER JOIN departments ON departments.deptID=modules.deptID WHERE departments.deptID=? && modules.level=?';
         var data = null;
         try {
-            data = await connection.general.promise().query(q);
+            data = await connection.general.promise().execute(q, [dept, lvl]);
         } catch (error) {
             console.log(error);
         }
         return data[0];
     }
 
+    static async getDepartmentDetails(id) {
+        var q = 'SELECT deptName FROM departments WHERE deptID=?';
+        var data = null;
+        try {
+            data = await connection.general.promise().execute(q, [id]);
+        } catch (error) {
+            console.log(error);
+        }
+        return data[0];
+    }
 }
 
 module.exports = DbHandler;
--- routes/index.js
+++ routes/index.js
@@ -18,7 +18,11 @@ router.get('/courses/modules', async (req, res) => {
         res.render('layouts/modules');
     else {
         var data = req.query;
-        res.render('layouts/test', { data });
+        const dept = await db.getDepartmentDetails(data.department);
+        const result = await db.getmodulesDetails(data.department, data.level);
+        console.log(dept);
+        console.log(result);
+        res.render('boilerplates/showModules', { dept, result });
     }
 });
 
