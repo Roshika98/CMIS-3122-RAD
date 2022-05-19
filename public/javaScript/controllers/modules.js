02e1a614f421bcfcc686560d131bc6e02e06b1e2
Roshika Perera <pereraroshika98@gmail.com>
2022-05-19 22:18:18 +0530

    modules content display added

--- public/javaScript/controllers/modules.js
+++ public/javaScript/controllers/modules.js
@@ -1,1 +1,55 @@
 console.log('I am modules');
+
+const dynamicContent = document.getElementById('dynamicContent');
+const submitBtn = document.getElementById('submitbtn');
+const deptSelect = document.getElementById('deptSelect');
+const lvlSelect = document.getElementById('lvlSelect');
+
+submitBtn.addEventListener('click', async (event) => {
+    event.preventDefault();
+    event.stopPropagation();
+    if (deptSelect.value == 'Select Department' || lvlSelect.value == 'Select Level of study') {
+        var opt = {
+            from: 'top',
+            align: 'center',
+            colorTheme: 'warning',
+            message: 'please select both department & level',
+            timer: 800,
+            icon: 'now-ui-icons travel_info'
+        };
+        nowuiDashboard.showNotification(opt);
+        return;
+    }
+    var params = new URLSearchParams([['department', `${deptSelect.value}`], ['level', `${lvlSelect.value}`]]);
+    try {
+        var response = await axios.get('https://localhost:3000/courses/admin/modules', { params });
+        addContent(response.data);
+
+        var opt = {
+            from: 'top',
+            align: 'center',
+            colorTheme: 'info',
+            message: 'modules view updated',
+            timer: 800,
+            icon: 'now-ui-icons ui-1_bell-53'
+        };
+        nowuiDashboard.showNotification(opt);
+    } catch (error) {
+        if (error.message == 'axios is not defined') {
+            var opt = {
+                from: 'top',
+                align: 'center',
+                colorTheme: 'danger',
+                message: 'Something Went Wrong :( , please check your connection',
+                timer: 800,
+                icon: 'now-ui-icons travel_info'
+            };
+            nowuiDashboard.showNotification(opt);
+        }
+    }
+
+});
+
+function addContent(content) {
+    dynamicContent.innerHTML = content;
+}
--- routes/index.js
+++ routes/index.js
@@ -42,35 +42,41 @@ router.get('/courses/modules', async (req, res) => {
 
 router.get('/courses/admin/homepage', (req, res) => {
     var layoutVar = { title: 'home', script: '' };
-
     res.render('admin/partials/home', { layoutVar, layout: 'admin/layout' });
 });
 
 router.get('/courses/admin/account', (req, res) => {
-    var layoutVar = { title: 'home', script: '/javaScript/controllers/user.js' };
+    var layoutVar = { title: 'account', script: '/javaScript/controllers/user.js' };
     res.render('admin/partials/user', { layoutVar, layout: 'admin/layout' });
 });
 
-router.get('/courses/admin/departments', (req, res) => {
-    var layoutVar = { title: 'home', script: '/javaScript/controllers/departments.js' };
+router.get('/courses/admin/departments', async (req, res) => {
+    var layoutVar = { title: 'departments', script: '/javaScript/controllers/departments.js' };
     var result = await db.getAllDepartments();
-    res.render('admin/partials/departments', { layoutVar, layout: 'admin/layout' });
+    console.log(result);
+    res.render('admin/partials/departments', { layoutVar, result, layout: 'admin/layout' });
 });
 
-router.get('courses/admin/departments/:id', (req, res) => {
+router.get('courses/admin/departments/:id', async (req, res) => {
     var { id } = req.params;
-
+    var result = await db.getDepartmentDetails(id);
 });
 
 
-router.get('/courses/admin/modules', (req, res) => {
-    var layoutVar = { title: 'home', script: '/javaScript/controllers/modules.js' };
-    res.render('admin/partials/modules', { layoutVar, layout: 'admin/layout' });
+router.get('/courses/admin/modules', async (req, res) => {
+    if (Object.keys(req.query).length == 0) {
+        var layoutVar = { title: 'modules', script: '/javaScript/controllers/modules.js' };
+        res.render('admin/partials/modules', { layoutVar, layout: 'admin/layout' });
+    } else {
+        console.log('hit');
+        var data = req.query;
+        const dept = await db.getDepartmentDetails(data.department);
+        const result = await db.getmodulesDetails(data.department, data.level);
+        var level = data.level;
+        console.log(dept);
+        res.render('admin/cardContent/modulesCard', { dept, level, result, layout: false });
+    }
 });
-
-
-
-
 
 
 
@@ -81,6 +87,13 @@ router.post('/courses/register', async (req, res) => {
     console.log('post request made');
     console.log(req.body);
 })
+
+
+//* ADMIN ROUTES--------------------------------------
+
+router.put('courses/admin/departments/:id', (req, res) => {
+
+});
 
 
 
