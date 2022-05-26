47006502f823c7c43334f86dd66aeda242da94da
Senaa4 <senaliniroma@gmail.com>
2022-05-26 13:02:00 +0530

    modified registration form

--- utility/pdfCreator.js
+++ utility/pdfCreator.js
@@ -4,14 +4,14 @@ const path = require('path');
 
 async function createPDF(level, data) {
     if (level <= 2) {
-        const pdfTemplate = await ejs.renderFile(path.join(__dirname, '/forms/'), { data }, { beautify: true, async: true });
+        const pdfTemplate = await ejs.renderFile(path.join(__dirname, '/forms/level.ejs'), { data }, { beautify: true, async: true });
         return pdfTemplate;
     } else if (level == 3) {
         if (data.personal.degreeDetails.degreeType == 1) {
-            const pdfTemplate = await ejs.renderFile(path.join(__dirname, '/forms/'), { data }, { beautify: true, async: true });
+            const pdfTemplate = await ejs.renderFile(path.join(__dirname, '/forms/general.ejs'), { data }, { beautify: true, async: true });
             return pdfTemplate;
         } else if (data.personal.degreeDetails.degreeType == 2) {
-            const pdfTemplate = await ejs.renderFile(path.join(__dirname, '/forms/'), { data }, { beautify: true, async: true });
+            const pdfTemplate = await ejs.renderFile(path.join(__dirname, '/forms/jointMajor.ejs'), { data }, { beautify: true, async: true });
             return pdfTemplate;
         } else {
             const pdfTemplate = await ejs.renderFile(path.join(__dirname, '/forms/'), { data }, { beautify: true, async: true });
@@ -19,7 +19,7 @@ async function createPDF(level, data) {
         }
     } else {
         if (data.personal.degreeDetails.degreeType == 2) {
-            const pdfTemplate = await ejs.renderFile(path.join(__dirname, '/forms/'), { data }, { beautify: true, async: true });
+            const pdfTemplate = await ejs.renderFile(path.join(__dirname, '/forms/jointMajor.ejs'), { data }, { beautify: true, async: true });
             return pdfTemplate;
         } else {
             const pdfTemplate = await ejs.renderFile(path.join(__dirname, '/forms/'), { data }, { beautify: true, async: true });
