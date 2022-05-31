b82c9e409e5c1508fec61457cd6df220055c438f
Roshika Perera <pereraroshika98@gmail.com>
2022-05-31 12:24:43 +0530

    user Routes wrapped around error handler

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,6 +3,8 @@ const db = require('../database/dbHandler');
 const router = express.Router();
 const pdfContent = require('../utility/pdfCreator');
 const puppeteer = require('puppeteer');
+const catchAsync = require('../utility/controllers/catchAsync');
+const user = require('../utility/controllers/userController');
 
 var pdfPage = null;
 
@@ -13,54 +15,18 @@ router.get('/', (req, res) => {
     res.render('user/partials/homepage', { layout: 'user/layout' });
 });
 
-router.get('/register', async (req, res) => {
-    if (Object.keys(req.query).length === 0) {
-        res.render('user/partials/course_registration', { layout: 'user/layout' });
-    }
-    else {
-        var q = req.query;
-        var data = await db.processSelection(q);
-        var lvl = q.level;
-        res.render('user/boilerplates/selection', { lvl, data, layout: false });
-    }
-});
+router.get('/register', catchAsync(user.getRegister));
 
-router.get('/modules', async (req, res) => {
-    if (Object.keys(req.query).length === 0)
-        res.render('user/partials/modules', { layout: 'user/layout' });
-    else {
-        var data = req.query;
-        const dept = await db.getDepartmentDetails(data.department);
-        const result = await db.getmodulesDetails(data.department, data.level);
-        res.render('user/boilerplates/showModules', { dept, result, layout: false });
-    }
-});
+router.get('/modules', catchAsync(user.getModules));
 
+router.get('/downloads', catchAsync(user.getDownloads));
 
-router.get('/downloads', async (req, res) => {
-    res.writeHead(200, { 'content-Type': 'application/pdf', 'Content-Disposition': 'attachment; filename="registrationform.pdf"' });
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    await page.setContent(`${pdfPage}`);
-    const buffer = await page.pdf({ format: "A4" });
-    await browser.close();
-    res.end(buffer);
-});
+router.get('/error', catchAsync(user.getError));
 
 
 //* POST ROUTES----------------------------------------------------------------------
 
-router.post('/register', async (req, res) => {
-    console.log('post request made');
-    console.log(req.body);
-    data = req.body;
-    data.semester1.mandatory.forEach(element => {
-        console.log(element);
-    });
-    var pdfTemplate = await pdfContent(data.personal.level, data);
-    pdfPage = pdfTemplate;
-    res.send('okay');
-});
+router.post('/register', catchAsync(user.postRegister));
 
 module.exports = router;
 
---
+++ utility/controllers/userController.js
@@ -0,0 +1,52 @@
+
+const getRegister = async (req, res) => {
+    if (Object.keys(req.query).length === 0) {
+        res.render('user/partials/course_registration', { layout: 'user/layout' });
+    }
+    else {
+        var q = req.query;
+        var data = await db.processSelection(q);
+        var lvl = q.level;
+        res.render('user/boilerplates/selection', { lvl, data, layout: false });
+    }
+};
+
+const getModules = async (req, res) => {
+    if (Object.keys(req.query).length === 0)
+        res.render('user/partials/modules', { layout: 'user/layout' });
+    else {
+        var data = req.query;
+        const dept = await db.getDepartmentDetails(data.department);
+        const result = await db.getmodulesDetails(data.department, data.level);
+        res.render('user/boilerplates/showModules', { dept, result, layout: false });
+    }
+};
+
+const getDownloads = async (req, res) => {
+    res.writeHead(200, { 'content-Type': 'application/pdf', 'Content-Disposition': 'attachment; filename="registrationform.pdf"' });
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.setContent(`${pdfPage}`);
+    const buffer = await page.pdf({ format: "A4" });
+    await browser.close();
+    res.end(buffer);
+};
+
+const getError = async (req, res, next) => {
+    throw new Error('This is an Error oh no!!!');
+};
+
+
+const postRegister = async (req, res) => {
+    console.log('post request made');
+    console.log(req.body);
+    data = req.body;
+    data.semester1.mandatory.forEach(element => {
+        console.log(element);
+    });
+    var pdfTemplate = await pdfContent(data.personal.level, data);
+    pdfPage = pdfTemplate;
+    res.send('okay');
+};
+
+module.exports = { getRegister, getModules, getDownloads, getError, postRegister };
